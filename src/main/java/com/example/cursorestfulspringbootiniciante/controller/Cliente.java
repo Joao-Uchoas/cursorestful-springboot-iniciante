88dd99128f0f5d57c7884ff6f153e0bb7117fc6e
Joao.Uchoas <jvuchoas1998@gmail.com>
2020-09-14 08:00:52 -0300

    Aula03-09-Parte01

---
+++ src/main/java/com/example/cursorestfulspringbootiniciante/controller/Cliente.java
@@ -0,0 +1,11 @@
+package com.example.cursorestfulspringbootiniciante.controller;
+
+public class Cliente {
+    
+    public int codigo;
+    public String nome;
+    public String endereco;
+    public double saldo;
+
+
+}
--- src/main/java/com/example/cursorestfulspringbootiniciante/controller/ClienteController.java
+++ src/main/java/com/example/cursorestfulspringbootiniciante/controller/ClienteController.java
@@ -1,5 +1,9 @@
 package com.example.cursorestfulspringbootiniciante.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,12 +11,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
-    
+
+    List <Cliente> clientes;
+
+    @PostConstruct
+    public void criarClientes(){
+        Cliente c1 = new Cliente(); 
+        Cliente c2 = new Cliente();
+        Cliente c3 = new Cliente();
+
+        c1.codigo = 1;
+        c1.endereco = "Rua X, 63";  
+        c1.nome = "Luana";
+        c1.saldo = 369;
+
+        c2.codigo = 2;
+        c2.endereco = "Rua Y, 94";  
+        c2.nome = "Ana";
+        c2.saldo = 581;
+
+        c3.codigo = 3;
+        c3.endereco = "Rua Z, 32";  
+        c3.nome = "Carla";
+        c3.saldo = 759;
+
+
+        clientes = Arrays.asList(c1,c2,c3);
+    }
 
     @GetMapping("/clientes")
-    public String getCliente()
+    public List<Cliente> getCliente()
     {
-        return "Vai retornar TODOS os clientes";
+        return clientes;
     }
 
     @GetMapping("/clientes/{codigo}")
